b574f5f65e086fdbd5119af14f83c02c02ff1da6
marciobento <m4rc10b3nt0@gmail.com>
2026-07-28 19:17:31 +0000

    faculdade desafio c nivel 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,42 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado[2], codigodacarta[4], nomedacidade[50];
+    int populacao, numerodepontosturisticos;
+    float area, pib;
 
   // Área para entrada de dados
+    printf("Digite uma letra de 'A' a 'H' (representando um dos oito estados): ");
+    scanf("%s", estado);
+
+    printf("Digite a letra do estado seguida de um número de 01 a 04: ");
+    scanf("%s", codigodacarta);
+
+    getchar(); // limpa o ENTER pendente
+
+    printf("Digite o nome da cidade: ");
+    fgets(nomedacidade, 50, stdin);
+
+    printf("Digite o número de habitantes da cidade: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade em quilômetros quadrados: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+    
+    printf("Digite a quantidade de pontos turísticos na cidade: ");
+    scanf("%d", &numerodepontosturisticos);
 
   // Área para exibição dos dados da cidade
-
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigodacarta);
+    printf("Nome da Cidade: %s", nomedacidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões\n", pib);
+    printf("Número de Pontos Turísticos: %d", numerodepontosturisticos);
+    
 return 0;
 } 
